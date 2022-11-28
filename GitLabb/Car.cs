95335e846129ebfe870e976ede67844cfb2c7a09
Adna Stupar <aasturp@gmail.com>
2022-11-28 15:31:26 +0100

    Added constructors and created an instance of the class

--- GitLabb/Car.cs
+++ GitLabb/Car.cs
@@ -3,8 +3,14 @@
 
 	public class Car
 	{
-			public string Make { get; set; }
-			public string Model { get; set; }
-	}
+        public string Make { get; set; }
+		public string Model { get; set; }
+        public Car(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+    }
 }
 
