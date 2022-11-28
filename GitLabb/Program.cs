10897bead3903b2252373d7a68ade9bd022f2f24
Shram Sardar <shramptm@gmail.com>
2022-11-28 15:58:56 +0100

    Add new

--- GitLabb/Program.cs
+++ GitLabb/Program.cs
@@ -6,6 +6,10 @@
         {
             var Car = new Car("Honda", "Civic");
             var Person = new Person("Alex", 25);
+            var Car2 = new Car("Toyota", "Corolla");
+
+
+
         }
 
     }
